0ee5f3d7db9f40daa29ec7eddce129fb99fafac1
mustafa0411 <mustafaalshebeeb@gmail.com>
2024-02-20 17:24:38 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CS 230L</h1>
+      <h2>Section - 01</h2>
+      <p>WVU ID: 800356480</p>
+      <p>Hi I am Mustafa Al-Shebeeb</p>
+      
+    </div>
+  );
+}
+
+export default App;
